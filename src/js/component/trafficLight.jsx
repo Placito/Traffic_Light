25a1dbbf46d797a3c75216628ac8cd977687b190
Mariana <101410421+Placito@users.noreply.github.com>
2023-07-17 20:21:09 +0000

    first part

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -7,27 +7,19 @@ const TrafficLight = () => {
 	const [light2, setLight2] = useState(false);
 	const [light3, setLight3] = useState(false);
 
-	function addLight1(){
-		setLight1(light1 => !light1);
+	function addLight(changeLight) {
+		changeLight(before => !before)
 	}
-	function addLight2(){
-		setLight2(light2 => !light2);
-	}
-	function addLight3(){
-		setLight3(light3 => !light3);
-	}
-	/*declaring the class selected, that has the property for given the glow effect in the light when you click*/
-	let toggleClass1 = light1 ? 'selected' : '';
-	let toggleClass2 = light2 ? 'selected' : '';
-	let toggleClass3 = light3 ? 'selected' : '';
 
 	return (
 	<div>
-		<div className="trafficTop"></div>
-		<div className="container">
-			<div className={`light1 ${toggleClass1}`} onClick={addLight1}></div>
-			<div className={`light2 ${toggleClass2}`} onClick={addLight2}></div>
-			<div className={`light3 ${toggleClass3}`} onClick={addLight3}></div>
+		<div>
+			<div className="trafficTop"></div>
+			<div className="container">
+				<div className={`light1 ${light1 ? 'selected' : ''}`} onClick={() => {addLight(setLight1)}}></div>
+				<div className={`light2 ${light2 ? 'selected' : ''}`} onClick={() => {addLight(setLight2)}}></div>
+				<div className={`light3 ${light3 ? 'selected' : ''}`} onClick={() => {addLight(setLight3)}}></div>
+			</div>
 		</div>
 	</div>
 	)	
